2aa802cb01af182b7805421750ab67d5c83e07d4
Armend Shala <79515966+armenddsh@users.noreply.github.com>
2022-02-22 00:36:13 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,82 @@
+
+/*
+  (1), 2, 3, 4, 5, ..., 12
+  1, (2), 3, 4, 5, ..., 12
+  1, 2, (3), 4, 5, ..., 12
+  1, ..., 3, (4), 5, ..., 12
+  1, ..., 4, (5), 6, ..., 12
+  1, ..., 5, (6), 7, ..., 12
+  1, ..., 6, (7), 8, ..., 12
+  1, ..., 7, (8), 9, ..., 12
+  1, ..., 8, (9), 10, ..., 12
+  1, ..., 8, 9, (10), 11, 12
+  1, ..., 8, 9, 10, (11), 12
+  1, ..., 8, 9, 10, 11, (12)
+  
+  1, ...,4,5,(6),7,8
+*/
+
+function getPages(currentPage, pageCount) {
+  debugger;
+  const maxPages = 7;
+  let pages = null;
+
+  if (currentPage <= pageCount && pageCount <= maxPages) {
+    pages = [];
+    for (let k = 0; k < pageCount; k++) {
+      const value = currentPage === k ? `(${k + 1})` : k + 1;
+      pages.push(value);
+    }
+  } else {
+    
+    pages = Array(maxPages).fill(null);
+    if ((currentPage > 3) && (currentPage < (pageCount - 2))) {
+      pages[0] = 1;
+      pages[1] = "...";
+      pages[2] = currentPage - 1;
+      pages[3] = `(${currentPage})`;
+      pages[4] = currentPage + 1;
+      pages[5] = "...";
+      pages[6] = pageCount;
+    } else if (currentPage > 3) {
+      pages[0] = 1;
+      pages[1] = "...";
+      
+      let n = 1;
+      let p = pageCount;
+      while(n <= 5) {
+        pages[maxPages - n] = p === currentPage ? `(${p})` : p;
+        n = n + 1;
+        p = p - 1;
+      }
+    } else if (currentPage < (pageCount - 2)) {
+      pages[maxPages - 1] = pageCount;
+      pages[maxPages - 2] = "...";
+
+      let n = 0;
+      let p = 1;
+      while(n < maxPages - 2) {
+        pages[n] = p === currentPage ? `(${p})` : p;
+        n = n + 1;
+        p = p + 1;
+      }
+    } 
+  }
+  return pages;
+}
+
+/*
+  Tests
+*/
+
+console.log(getPages(2, 5));
+console.log(getPages(4, 7));
+console.log(getPages(5, 8));
+console.log(getPages(6, 8));
+console.log(getPages(7, 8));
+console.log(getPages(8, 8));
+
+const pages = 100;
+for(let k = 0; k <= pages; k++) {
+   console.log(getPages(k, pages)); 
+}
